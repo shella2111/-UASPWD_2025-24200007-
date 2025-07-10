6f5c5cade72ef62ecb16be0b602dae3a3ccf811c
Nolanda Freeshella <nfreeshella@gmail.com>
2025-07-10 09:06:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-
+const { parse } = require('querystring');
 const publicDir = path.join(__dirname, 'public');
-const port = 8000;
+const port = 5000;
 
+const mysql = require('mysql');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -17,9 +18,9 @@ db.connect((err) => {
     console.log("koneksi database gagal");
     process.exit();
   }
+
   console.log("database terhubung!");
 });
-
 const server = http.createServer((req, res) => {
   if(req.method === 'GET'){
     const filePath = req.url === '/' ? '/index.html' : req.url;
@@ -29,26 +30,26 @@ const server = http.createServer((req, res) => {
       const contentType = ext === '.css' ? 'text/css':
                           ext === '.js' ? 'text/javascript':
                           ext === '.html' ? 'text/html' : 'text/plain';
+
       res.writeHead(200, { 'content-type': contentType});
       res.end(content);
     });
-
-  }else if(req.method === 'POST' && req.url === '/contact'){
+  }else if(req.method === 'POST' && req.url === '/daftar'){
     let body = '';
     req.on('data', chunk => body += chunk);
     req.on('end', () => {
+      //proses di sini!
       const parsed = parse(body);
-      const {name, email, item} = parsed;
-      const sql = 'insert into context (name, email, item) values(?, ?, ?)';
-      db.query(sql, [name, email, item], (err) => {
+      const {name, password} = parsed;
+      const sql = 'insert into formulir (name, password, item) values(?, ?)';
+      db.query(sql, [name, password], (err) => {
         if(err){
-          console.log("gagal menyimpan ke database");
+          console.log("gagal simpan ke db");
           res.writeHead(500, {'content-type': 'text/plain'});
-          return res.end("gagal menyimpan ke database");
+          return res.end("gagal simpan ke db");
         }
-
         res.writeHead(200, {'content-type': 'text/plain'});
-        return res.end('berhasil menyimpan ke database!');
+        return res.end('berhasil simpan ke db');
       });
     });
   }
