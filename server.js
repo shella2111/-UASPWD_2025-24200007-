ae386317b0acb4f94df797d97a6fb9e11a5a417f
Nolanda Freeshella <nfreeshella@gmail.com>
2025-07-10 09:19:27 +0700

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const { parse } = require('querystring');
 const publicDir = path.join(__dirname, 'public');
-const port = 5000;
+const port = 1100;
 
 const mysql = require('mysql');
 const db = mysql.createConnection({
@@ -15,12 +15,13 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if(err){
-    console.log("koneksi database gagal");
+    console.log("koneksi ke database gagal");
     process.exit();
   }
 
-  console.log("database terhubung!");
+  console.log("database berhasil terhubung!");
 });
+
 const server = http.createServer((req, res) => {
   if(req.method === 'GET'){
     const filePath = req.url === '/' ? '/index.html' : req.url;
@@ -34,22 +35,23 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, { 'content-type': contentType});
       res.end(content);
     });
+
   }else if(req.method === 'POST' && req.url === '/daftar'){
     let body = '';
     req.on('data', chunk => body += chunk);
     req.on('end', () => {
       //proses di sini!
       const parsed = parse(body);
-      const {name, password} = parsed;
-      const sql = 'insert into formulir (name, password, item) values(?, ?)';
-      db.query(sql, [name, password], (err) => {
+      const {name, email, item} = parsed;
+      const sql = 'insert into formulir (name, email, item) values(?, ?)';
+      db.query(sql, [name, email, item], (err) => {
         if(err){
-          console.log("gagal simpan ke db");
+          console.log("gagal menyimpan ke database");
           res.writeHead(500, {'content-type': 'text/plain'});
-          return res.end("gagal simpan ke db");
+          return res.end("gagal menyimpan ke database");
         }
         res.writeHead(200, {'content-type': 'text/plain'});
-        return res.end('berhasil simpan ke db');
+        return res.end('berhasil menyimpan ke database');
       });
     });
   }
